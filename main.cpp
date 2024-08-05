e9a4f325a5120545fa5ddb471427888f45328659
minhhitgub <hungminhnguyena1@gmail.com>
2024-08-05 09:39:14 +0700

    updated

--- main.cpp
+++ main.cpp
@@ -31,6 +31,7 @@ std::vector<obstacle> obstacles;
 void initObstacle()
 {
     obstacles.push_back(obstacle {SDL_Rect{200, GROUND - 100, 100, 100}});
+    obstacles.push_back(obstacle {SDL_Rect{500, GROUND - 150, 100, 150}});
 }
 
 
@@ -92,34 +93,45 @@ void update(Player& player)
 }
 
 bool checkCollision(const SDL_Rect& player, const SDL_Rect& obs) {
-    if(player.x + player.w <= obs.x || player.x >= obs.x + obs.w || player.y + player.h <= obs.y || player.y >= obs.y + obs.h)
-    {
-        return false;
-    }
-    else return true;
+    return SDL_HasIntersection(&player, &obs);
 }
 
-void handleCollisions(Player& player) {
+
+void handleCollision(Player& player) {
     SDL_Rect playerRect = { player.x, player.y, PLAYER_WIDTH, PLAYER_HEIGHT };
+
     for (const auto& obs : obstacles) {
         if (checkCollision(playerRect, obs.rect)) {
-                if (playerRect.y + playerRect.h > obs.rect.y) {
-                player.y = obs.rect.y - PLAYER_HEIGHT;
-                player.vy = 0;
-                player.isJumping = false;
-            } else if ( playerRect.y < obs.rect.y + obs.rect.h) {
-                player.y = obs.rect.y + obs.rect.h;
-                player.vy = 0;
-            }
-            if ( playerRect.x + playerRect.w > obs.rect.x) {
-                player.x = obs.rect.x - PLAYER_WIDTH;
-            } else if ( playerRect.x < obs.rect.x + obs.rect.w) {
-                player.x = obs.rect.x + obs.rect.w;
+
+            if (playerRect.y + playerRect.h > obs.rect.y && playerRect.y < obs.rect.y + obs.rect.h) {
+                if (player.vy > 0 && playerRect.y + playerRect.h - player.vy <= obs.rect.y) {
+
+                    player.y = obs.rect.y - PLAYER_HEIGHT;
+                    player.vy = 0;
+                    player.isJumping = false;
+                } else if (player.vy < 0 && playerRect.y >= obs.rect.y + obs.rect.h - player.vy) {
+
+                    player.y = obs.rect.y + obs.rect.h;
+                    player.vy = 0;
+                }
             }
 
+    playerRect = { player.x, player.y, PLAYER_WIDTH, PLAYER_HEIGHT };
+
+            if (playerRect.x + playerRect.w > obs.rect.x && playerRect.x < obs.rect.x + obs.rect.w) {
+                if (player.vx > 0 && playerRect.x + playerRect.w - player.vx <= obs.rect.x) {
+
+                    player.x = obs.rect.x - PLAYER_WIDTH;
+                }
+                else if (player.vx < 0 && playerRect.x >= obs.rect.x + obs.rect.w + player.vx) {
+
+                    player.x = obs.rect.x + obs.rect.w;
+                }
+            }
         }
     }
 }
+
 
 void render(SDL_Renderer* renderer, SDL_Texture* backgroundTexture, Player& player)
 {
@@ -197,9 +209,9 @@ int main(int argc, char* argv[])
     while (running) {
         processInput(player, running);
         update(player);
-        handleCollisions(player);
+        handleCollision(player);
         render(renderer, backgroundTexture , player);
-        SDL_Delay(16);
+        SDL_Delay(16.67);
     }
 
     SDL_DestroyTexture(backgroundTexture);
